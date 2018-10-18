579e00d93dec7e249c656a93bc22e27dbea330b7
Almarvds <a.r.v.d.stappen@student.tue.nl>
2018-10-18 14:13:35 +0200

    changes to everything
    new file system
    better vuforia integration
    newer vuforia API
    class system added
    resource gathering added

---
+++ AR math game/Assets/scripts/MaterialRole.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaterialRole : MonoBehaviour {
+
+    int Occupation;
+    Image materialImage;
+    public Sprite Woodcutting; 
+    public Sprite Painting; 
+    public Sprite Designing; 
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Occupation ==1) {
+            materialImage.sprite = Woodcutting;
+        } else if (Occupation == 2)
+        {
+            materialImage.sprite = Painting;
+        } else {
+            materialImage.sprite = Designing;
+        }
+	}
+}
---
+++ AR math game/Assets/scripts/Materialcollected.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Materialcollected : MonoBehaviour {
+
+    public Button btn;
+    public Text MaterialScore;
+    public int Scoring;
+    public int MaterialsNeeded;
+
+	// Use this for initialization
+	void Start () {
+        btn.onClick.AddListener(ButtonClick);
+        MaterialScore.text = Scoring + "/" + MaterialsNeeded;
+    }
+	
+    void ButtonClick()
+    {
+        Debug.Log("button clicked");
+        Scoring++;
+        MaterialScore.text = Scoring + "/" + MaterialsNeeded;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
